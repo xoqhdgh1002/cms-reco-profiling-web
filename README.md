21e1f8a2544a867e207f0096c01ae2cfb6f6f255
Joosep Pata <joosep.pata@cern.ch>
2021-03-10 19:19:05 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#
+
+Contents of the http://cms-reco-profiling.web.cern.ch page.
+
+
+```
+[lxplus]$ ./deploy.sh
+``` 
---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+DEPLOY_DIR=/eos/project/c/cmsweb/www/reco-prof
+
+#deploy the HTML
+cp -R web $DEPLOY_DIR/
+
+#Deploy igprof navigator
+mkdir $DEPLOY_DIR/cgi-bin
+mkdir $DEPLOY_DIR/cgi-bin/data
+wget https://raw.githubusercontent.com/cms-externals/igprof/51fbb32/src/igprof-navigator -O $DEPLOY_DIR/cgi-bin/igprof-navigator
+chmod +x $DEPLOY_DIR/cgi-bin/igprof-navigator
+
+#Deploy circles
