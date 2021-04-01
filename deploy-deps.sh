5e7da9aefd2899188831788d0957b1a50fb9bc24
Joosep Pata <joosep.pata@cern.ch>
2021-04-01 08:38:17 +0200

    use master version of igprof with /1 fix

--- deploy-deps.sh
+++ deploy-deps.sh
@@ -3,13 +3,13 @@ set -e
 
 DEPLOY_DIR=/eos/project/c/cmsweb/www/reco-prof
 DATA_DIR=/eos/cms/store/user/cmsbuild/profiling
-IGPROF_COMMIT=c6882f4d8e39893d71466ea23da643dafb60a496
+IGPROF_COMMIT=03d16c15bd970256affcfde9e510a8404856b233
 CIRCLES_COMMIT=f5321bf6352caeecc681c877dea473e5a61d647f
 
 #Deploy igprof navigator
 [ ! -d ${DEPLOY_DIR}/cgi-bin ] && mkdir ${DEPLOY_DIR}/cgi-bin
 [ ! -d ${DEPLOY_DIR}/cgi-bin/data ] && mkdir ${DEPLOY_DIR}/cgi-bin/data
-wget https://raw.githubusercontent.com/cms-externals/igprof/${IGPROF_COMMIT}/src/igprof-navigator -O ${DEPLOY_DIR}/cgi-bin/igprof-navigator
+wget https://raw.githubusercontent.com/igprof/igprof/${IGPROF_COMMIT}/src/igprof-navigator -O ${DEPLOY_DIR}/cgi-bin/igprof-navigator
 chmod +x ${DEPLOY_DIR}/cgi-bin/igprof-navigator
 
 #Deploy circles
