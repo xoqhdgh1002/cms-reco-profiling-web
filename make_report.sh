c50ec26dd20342eb15465408e4674ad8f4ef336d
Bongho Tae <btae@cern.ch>
2021-04-14 13:29:01 +0200

    Develop profiling source code

---
+++ make_report.sh
@@ -0,0 +1,1 @@
+igprof-analyse  -v -d -g /eos/cms/store/user/cmsbuild/profiling/data/${1}/slc7_amd64_gcc*/23434.21/step${2}_igprof${3}.gz >& /eos/project/c/cmsweb/www/reco-prof/results/RES/RES_${1}_step${2}_${3}.res
