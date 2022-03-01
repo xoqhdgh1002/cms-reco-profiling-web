fc161d8bf5e2f92722005e336d922840fc1f6914
Bongho Tae <btae@cern.ch>
2022-03-01 08:37:24 +0100

    Test for jenkins

---
+++ jenkins/make_get_time_memory_summary.py
@@ -0,0 +1,35 @@
+import os
+import yaml
+import Log_check
+
+DATA_DIR = '/eos/cms/store/user/cmsbuild/profiling/data/' 
+
+def parse_args():
+
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--profile-data", type=str, default=DATA_DIR, help="profiling data location")
+	parser.add_argument("--release", type=str, help="CMSSW release", default=None)
+	parser.add_argument("--architecture", type=str, help="architecture for release", default=None)
+	parser.add_argument("--workflow", type=str, help="workflow", default=None)
+	args = parser.parse_args()
+	return args
+
+if __name__ == "__main__":
+
+	Log = Log_check.TimeMem()
+
+	args = parse_args()
+
+	release = args.release
+	architecture = args.architecture
+	workflow = args.workflow
+
+	for step in ['step3','step4','step5']:
+	
+		base = os.path.join(DATA_DIR,release,architecture,workflow)
+		tmi = os.path.join(base,"{}_TimeMemoryInfo.log".format(step))
+	
+		if os.path.isfile(tmi):
+			Log.Get_TimeMem(tmi)
+			Log.summary("{}.txt".format(step))
