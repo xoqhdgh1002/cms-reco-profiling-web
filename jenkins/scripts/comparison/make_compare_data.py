793fe78a0a6d370a3031ce4546fd33e734f8ca38
Bongho Tae <btae@cern.ch>
2022-03-10 05:36:40 +0100

    Update jenkins scripts for comparison

---
+++ jenkins/scripts/comparison/make_compare_data.py
@@ -0,0 +1,39 @@
+import yaml
+import os
+import sys
+
+data_path = '/eos/cms/store/user/cmsbuild/profiling/data/'
+cmssw = os.listdir(data_path)
+cmssw.sort(key = lambda x: (x.split('_')[1:4],10-len(x.split('_')),len(x)))
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--release", type=str, help="CMSSW release", default=None)
+parser.add_argument("--workflow", type=str, help="workflow", default=None)
+args = parser.parse_args()
+
+new = args.release
+old = cmssw[cmssw.index(new)-1]
+new_architecture = os.listdir(os.path.join(data_path,new))[0]
+old_architecture = os.listdir(os.path.join(data_path,old))[0]
+workflow = args.workflow
+
+if sys.argv[1] == "N03_timeDiffFromReport.sh":
+	result_path = 'TimeDiff/'
+
+if sys.argv[1] == "N04_compareProducts.sh":
+	result_path = 'CompProd/'
+
+for step in ['step3','step4','step5']:
+	
+	new_data = os.path.join(data_path,new,new_architecture,'{}_TimeMemoryInfo.log'.format(step))
+	old_data = os.path.join(data_path,old,old_architecture,'{}_TimeMemoryInfo.log'.format(step))
+
+	os.makedirs(result_path,exist_ok = True)
+	if os.path.isfile(file_path_1) and os.path.isfile(file_path_2):
+		os.system("source ./{8} {0}{2}/{3}/{6}/{7} {0}{4}/{5}/{6}/{7} > {1}{4}/{5}/{6}/{7}.txt".format(
+		data_path,result_path,
+		old,old_architecture,
+		new,new_architecture,
+		workflow,step,sys.argv[1]))
+
