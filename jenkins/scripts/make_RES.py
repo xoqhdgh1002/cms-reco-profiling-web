8562fbe4a58bb50fb4439b03fad0128456d4eff3
Bongho Tae <36912292+xoqhdgh1002@users.noreply.github.com>
2022-07-07 16:04:47 +0900

    Propagate 12_4,5 in the web page (#64)
    * upload for #63 and RES
    
    * upload the make_RES.py
    
    * upload the make_RES.py v2
    
    * python to python3
    
    * Propagate 12_4,5 in the web page
    
    Co-authored-by: Joosep Pata <joosep.pata@gmail.com>

--- jenkins/scripts/make_RES.py
+++ jenkins/scripts/make_RES.py
@@ -1,10 +1,7 @@
-import yaml
 import os
 import sys
 
-yaml_file = yaml.load(open('/eos/user/b/btae/www/Service_Work/test/cms-reco-profiling-web/test/yaml/data.yaml','r'))
-
-data_path = '/eos/cms/store/user/cmsbuild/profiling/data'
+DATA_DIR = '/eos/cms/store/user/cmsbuild/profiling/data'
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -20,7 +17,7 @@ workflow = args.workflow
 
 for step in ['step3','step4','step5']:
 	data_dir = "{0}/{1}/{2}".format(release,architecture,workflow)
-	gz_list = [x for x in os.listdir("{0}/{1}/{2}/{3}".format(data_path,release,architecture,workflow)) if "gz" in x and step in x]
+	gz_list = [x for x in os.listdir("{0}/{1}/{2}/{3}".format(DATA_DIR,release,architecture,workflow)) if "gz" in x and step in x]
 	if len(gz_list)==0:
 		continue
 	print(release,architecture,workflow,step)
@@ -28,8 +25,8 @@ for step in ['step3','step4','step5']:
 		if "CPU" in gz:
 			if os.path.isfile("/eos/project/c/cmsweb/www/reco-prof/results/RES/{0}/{1}_cpu.res".format(data_dir,step)):
 				continue
-			os.system("source ./make_res.sh 0 {0}/{1}/{2} /eos/project/c/cmsweb/www/reco-prof/results/RES/{1}/{3}_cpu.res".format(data_path,data_dir,gz,step))
+			os.system("source ./make_res.sh 0 {0}/{1}/{2} /eos/project/c/cmsweb/www/reco-prof/results/RES/{1}/{3}_cpu.res".format(DATA_DIR,data_dir,gz,step))
 		elif "MEM" in gz:
 			if os.path.isfile("/eos/project/c/cmsweb/www/reco-prof/results/RES/{0}/{1}_mem_{2}.res".format(data_dir,step,gz.split(".")[1])):
 				continue
-			os.system("source ./make_res.sh 1 {0}/{1}/{2} /eos/project/c/cmsweb/www/reco-prof/results/RES/{1}/{3}_mem_{4}.res".format(data_path,data_dir,gz,step,gz.split(".")[1]))
+			os.system("source ./make_res.sh 1 {0}/{1}/{2} /eos/project/c/cmsweb/www/reco-prof/results/RES/{1}/{3}_mem_{4}.res".format(DATA_DIR,data_dir,gz,step,gz.split(".")[1]))
