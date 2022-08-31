c58ae8d35c60dba9e589e25a082929991ab7fbd9
Bongho Tae <btae@cern.ch>
2022-08-31 11:43:35 +0200

    update the make_jenkins_env.py

---
+++ jenkins/scripts/pre-environment/make_jenkins_env.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from shutil import copyfile
+import yaml
+
+data_path = '/eos/cms/store/user/cmsbuild/profiling/data/'
+results_path = '/eos/project/c/cmsweb/www/reco-prof/results/' 
+cmssw = os.listdir(data_path)
+
+workflow ={}
+
+for result_dir in os.listdir(results_path):
+	if result_dir == 'comp_igprof' or result_dir == 'summary_plot_html':
+		continue
+	for i in cmssw:
+		gcc = os.listdir(data_path + i)[0]
+		workflow[i] = {"gcc":gcc,"workflow":os.listdir(data_path + i +'/' + gcc)}
+		for j in os.listdir(data_path + i + '/' + gcc):
+			child_dir = results_path + result_dir + '/' + i + '/' + gcc + '/' + j
+			os.makedirs(child_dir,exist_ok=True) 
+			os.system("cp {0}{1}/{2}/{3}/cmdLog_profiling.sh {4}cmdlog/{1}/{2}/{3}/cmdLog_profiling.txt".format(data_path,i,gcc,j,results_path))
