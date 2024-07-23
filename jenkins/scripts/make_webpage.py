83ada7ff55b8471e48d2675a885690e6b196bc14
Bongho Tae <btae@cern.ch>
2024-07-23 14:40:56 +0200

    update make_webpage.py

--- jenkins/scripts/make_webpage.py
+++ jenkins/scripts/make_webpage.py
@@ -5,18 +5,18 @@ import yaml
 recent_rel = sys.argv[1]
 
 local_path = '../'
-data_path = '/eos/cms/store/user/cmsbuild/profiling/data/'
-result_path = '/eos/project/c/cmsweb/www/reco-prof/results/'
+result_path = '/eos/project/c/cmsweb/www/reco-prof/results'
 result_address = 'http://cms-reco-profiling.web.cern.ch/cms-reco-profiling/results/'
 
-cmssw_list = os.listdir(data_path)
+cmdlog_path = os.path.join(result_path,'cmdlog')
+cmssw_list = os.listdir(cmdlog_path)
 cmssw_list.sort(key = lambda x: (x.split('_')[1:4],10-len(x.split('_')),len(x)))
 
 cmssw_yaml = {} 
 
 for i in cmssw_list:
-        gcc = os.listdir(data_path + i)[0]
-        cmssw_yaml[i] = {"gcc":gcc,"workflow":os.listdir(data_path + i +'/' + gcc)}
+        gcc = os.listdir(os.path.join(cmdlog_path,i))[0]
+        cmssw_yaml[i] = {"gcc":gcc,"workflow":os.listdir(os.path.join(cmdlog_path,i,gcc))}
 
 mother_version = ''
 data_type = {"step2":"step2_RAW(DIGI)","step3":"step3_AOD(RECO)","step4":"step4_MiniAOD(PAT)","step5":"step5_NanoAOD"}
@@ -47,7 +47,7 @@ for cmssw in cmssw_list:#Version Loop
 """	<h2>{0}_X <font size="4em"></font></h2>""".format(mother_version)
 		)
 
-		for sum_plot in os.listdir(result_path + 'summary_plot_html'):#Summary plot Loop
+		for sum_plot in os.listdir(os.path.join(result_path,'summary_plot_html')):#Summary plot Loop
 
 			if mother_version in sum_plot:#Input Summary plot for same version
 				print(
@@ -74,7 +74,7 @@ for cmssw in cmssw_list:#Version Loop
 <br>"""
 		)#End of Summary plot Loop
 
-		for sum_plot in [x for x in os.listdir(result_path + 'circles/web/hist/') if 'Eventsize' in x ]:#Summary plot Loop
+		for sum_plot in [x for x in os.listdir(os.path.join(result_path,'circles/web/hist/')) if 'Eventsize' in x ]:#Summary plot Loop
 
 			if '_'.join(mother_version.split('_')[1:]) in sum_plot:#Input Summary plot for same version
 				print(
@@ -103,7 +103,7 @@ for cmssw in cmssw_list:#Version Loop
 
 
 		if mother_version == '_'.join(recent_rel.split('_')[0:3]):
-			for sum_plot in [x for x in os.listdir(result_path + 'circles/web/hist/') if 'Recent' in x ]:#Summary plot Loop
+			for sum_plot in [x for x in os.listdir(os.path.join(result_path,'circles/web/hist/')) if 'Recent' in x ]:#Summary plot Loop
 	
 				if 'Recent' in sum_plot:#Input Summary plot for same version
 					print(
@@ -134,7 +134,7 @@ for cmssw in cmssw_list:#Version Loop
 	
 	for workflow in cmssw_yaml[cmssw]['workflow']:#Workflow Loop
 
-		TMS_file = os.listdir('{0}Time_Mem_Summary/{1}/{2}/{3}'.format(result_path,cmssw,gcc,workflow))
+		TMS_file = os.listdir('{0}/Time_Mem_Summary/{1}/{2}/{3}'.format(result_path,cmssw,gcc,workflow))
 
 		if len(TMS_file) == 0:#Select empty TMS dir
 
@@ -145,7 +145,7 @@ for cmssw in cmssw_list:#Version Loop
 		<br>
 		<ul>""".format(cmssw,workflow))
 		
-		if "cmdLog_profiling.sh" in os.listdir("/eos/cms/store/user/cmsbuild/profiling/data/{0}/{1}/{2}".format(cmssw,gcc,workflow)):
+		if "cmdLog_profiling.txt" in os.listdir("/{0}/cmdlog/{1}/{2}/{3}".format(result_path,cmssw,gcc,workflow)):
 			print("""
 			<li>
 			<a href="{0}cmdlog/{1}/{2}/{3}/cmdLog_profiling.txt" title="cmdLog">[cmdLog]</a>""".format(result_address,cmssw,gcc,workflow))
@@ -164,7 +164,7 @@ for cmssw in cmssw_list:#Version Loop
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------
 			if os.path.isdir("/eos/project/c/cmsweb/www/reco-prof/cgi-bin/data/releases/{}".format(from_cgi_path)):
-				res_list = os.listdir("{0}RES/{1}".format(result_path,from_data_path))
+				res_list = os.listdir("{0}/RES/{1}".format(result_path,from_data_path))
 				if len(res_list) != 0:
 					for res in [i for i in res_list if step in i]:
 						if "cpu" in res:
@@ -178,14 +178,14 @@ for cmssw in cmssw_list:#Version Loop
 						)
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------
-			if os.path.isfile("{0}TimeDiff/{1}{2}.txt".format(result_path,from_data_path,step)) or os.path.isfile("{0}CompProd/{1}{2}.txt".format(result_path,from_data_path,step)):
+			if os.path.isfile("{0}/TimeDiff/{1}{2}.txt".format(result_path,from_data_path,step)) or os.path.isfile("{0}/CompProd/{1}{2}.txt".format(result_path,from_data_path,step)):
 
 				print("""
 			<li>Comparison :
 			<ul>""")
 
-			if os.path.isfile("{0}TimeDiff/{1}{2}.txt".format(result_path,from_data_path,step)):
-				f = open("{0}TimeDiff/{1}{2}.txt".format(result_path,from_data_path,step))
+			if os.path.isfile("{0}/TimeDiff/{1}{2}.txt".format(result_path,from_data_path,step)):
+				f = open("{0}/TimeDiff/{1}{2}.txt".format(result_path,from_data_path,step))
 				timediff = f.readlines()[-1]
 
 				print("""
@@ -193,23 +193,23 @@ for cmssw in cmssw_list:#Version Loop
 				<a href="{0}TimeDiff/{1}{2}.txt" title="TimeDiff">[TimeDiff]</a><span style=" font: Arial; font-size: small; color: green;"> preview: {3} s/ev ==> {4} s/ev </span>
 				</li>""".format(result_address,from_data_path,step,timediff.split()[2],timediff.split()[5])				)
 
-			if os.path.isfile("{0}CompProd/{1}{2}.txt".format(result_path,from_data_path,step)):
-				f = open("{0}CompProd/{1}{2}.txt".format(result_path,from_data_path,step))
+			if os.path.isfile("{0}/CompProd/{1}{2}.txt".format(result_path,from_data_path,step)):
+				f = open("{0}/CompProd/{1}{2}.txt".format(result_path,from_data_path,step))
 				compprod = f.readlines()[-1]
 
 				print("""
 				<li>
-				<a href="{0}CompProd/{1}{2}.txt" title="Compare_Out_Prod">[CompareOutProd (edmEventSize)]</a><span style=" font: Arial; font-size: small; color: red;"> preview: {3} ==> {4} </span>
+				<a href="{0}/CompProd/{1}{2}.txt" title="Compare_Out_Prod">[CompareOutProd (edmEventSize)]</a><span style=" font: Arial; font-size: small; color: red;"> preview: {3} ==> {4} </span>
 				</li>""".format(result_address,from_data_path,step,compprod.split()[1],compprod.split()[2]))
 
-			if os.path.isfile("{0}TimeDiff/{1}{2}.txt".format(result_path,from_data_path,step)) or os.path.isfile("{0}CompProd/{1}{2}.txt".format(result_path,from_data_path,step)):
+			if os.path.isfile("{0}/TimeDiff/{1}{2}.txt".format(result_path,from_data_path,step)) or os.path.isfile("{0}/CompProd/{1}{2}.txt".format(result_path,from_data_path,step)):
 
 				print("""
 			</ul>""")
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------
-			if os.path.isdir("{0}comp_igprof/html/{1}".format(result_path,from_cgi_path)):
-				comp_igprof = os.listdir("{0}comp_igprof/html/{1}".format(result_path,from_cgi_path))
+			if os.path.isdir("{0}/comp_igprof/html/{1}".format(result_path,from_cgi_path)):
+				comp_igprof = os.listdir("{0}/comp_igprof/html/{1}".format(result_path,from_cgi_path))
 				if len(comp_igprof) != 0:
 					print("""
 			<li>Igprof comparison :
@@ -235,22 +235,14 @@ for cmssw in cmssw_list:#Version Loop
 					print("""
 			<li>Circle (Pie chart) :
 			<a href="http://cms-reco-profiling.web.cern.ch/cms-reco-profiling/{0}" title="Circle">[Circle]</a>
-			</li>""".format("circles/piechart.php?local=false&dataset="+cmssw+"%2F"+gcc+"%2F"+workflow+"%2F"+step+"_circles&resource=time_real&colours=default&groups=package&threshold=0")
+			</li>""".format("circles/piechart.php?local=false&dataset="+cmssw+"%2F"+gcc+"%2F"+workflow+"%2F"+step+"_circles&resource=time_real&colours=default&groups=reco_PhaseII_private&threshold=0")
 				)
 					if os.path.isfile("{0}/circles/web/data/{1}_{2}_{3}_eventSize.json".format(result_path,cmssw,workflow,step)):
-						if step == "step3":
 							print("""
 			<li>EventSizeCircle (pie chart) :
-			<a href="https://cms-reco-profiling.web.cern.ch/cms-reco-profiling/results/circles/web/eventsize.php?local=false&dataset={0}_{1}_{2}_eventSize&resource=size_uncom&colours=default&groups=reco_PhaseII&threshold=0" title="EventSize">[EventSize]</a>
+			<a href="https://cms-reco-profiling.web.cern.ch/cms-reco-profiling/results/circles/web/eventsize.php?local=false&dataset={0}_{1}_{2}_eventSize&resource=size_uncom&colours=default&groups=Group_{2}_23Sep_v1&threshold=0" title="EventSize">[EventSize]</a>
 			</li>""".format(cmssw,workflow,step)
 			)
-						else:
-							print("""
-			<li>EventSizeCircle (pie chart) :
-			<a href="https://cms-reco-profiling.web.cern.ch/cms-reco-profiling/results/circles/web/eventsize_stepx.php?local=false&dataset={0}_{1}_{2}_eventSize&resource=size_uncom&colours=default&groups=reco_PhaseII&threshold=0" title="EventSize">[EventSize]</a>
-			</li>""".format(cmssw,workflow,step)
-			)
-
 			print(
 "		</ul>"
 )
