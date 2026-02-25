9f75d24a139d4c4876cc6cffec7df4aba776fe14
Bongho Tae <btae@cern.ch>
2026-02-25 06:29:01 +0100

    fixed make_webpage.py

--- jenkins/scripts/make_webpage.py
+++ jenkins/scripts/make_webpage.py
@@ -162,7 +162,6 @@ for cmssw in cmssw_list:#Version Loop
 				<a href="{0}Time_Mem_Summary/{1}{2}.txt" title="getTimeMemSummary">[getTimeMemSummary]</a>""".format(result_address,from_data_path,step)
 			)
 			print("""
-				<li>
 				<a href="{0}Time_Mem_Summary/{1}memory_report_{2}.txt" title="memory_report">[memory_report]</a>""".format(result_address,from_data_path,step)
 			)
 
